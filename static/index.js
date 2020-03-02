39380ce9337de0db85595bb7205febb602549a7f
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-03-02 14:53:20 +0800

    rename vars

--- static/index.js
+++ static/index.js
@@ -3,11 +3,11 @@ const mime = require('mime')
 const fs = require('fs')
 
 module.exports = (url) => async (ctx, next) => {
-  const rpath = ctx.request.path
-  if (rpath.startsWith(url)) {
-    const fp = path.resolve(__dirname, rpath.substring(url.length))
+  const reqPath = ctx.request.path
+  if (reqPath.startsWith(url)) {
+    const fp = path.resolve(__dirname, reqPath.substring(url.length))
     if (fs.existsSync(fp)) {
-      ctx.response.type = mime.getType(rpath)
+      ctx.response.type = mime.getType(reqPath)
       ctx.response.body = fs.readFileSync(fp, { encoding: 'utf8' })
     } else {
       ctx.response.status = 404
